72bb79a8f59b834120760b1f1c0215cfcf15810f
Gabriel Chiappa <Gabriel@GGUnifiedTech.com>
2025-09-29 14:35:37 -0400

    fix: Add TypeScript declarations for CSS imports
    - Create global.d.ts with CSS module type declarations
    - Resolves TypeScript error 2882 for './globals.css' import
    - Adds support for .css, .scss, .sass, .less files
    - Includes CSS modules declarations for better type safety

---
+++ src/types/global.d.ts
@@ -0,0 +1,40 @@
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: any;
+  export default content;
+}
+
+// For CSS modules
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.less' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
